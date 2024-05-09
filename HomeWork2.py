9aa5d5e0cbe2976eac84bd57bff93ccfba9adacb
WatsonFan <30586391+watosnfan@users.noreply.github.com>
2024-05-09 19:22:24 +0800

    watson
    家庭作業0509

---
+++ HomeWork2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for b in range(1,10):
+    for a in range(9,1,-1):
+        print(a,"*",b,"=",a*b,sep="",end="\t")
+        
+    print()    
